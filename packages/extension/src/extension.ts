31a96d43722530f3f57eb04df8bc853074012fd2
HiMeditator <hironin@foxmail.com>
2025-04-01 14:23:30 +0800

    feat(extension): add basic functionality and localization support
    - Add commands
    - Implement localization support
    - Refactor extension activation to include new features

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
-
+import {l10n} from 'vscode';
 import { MainViewProvider } from './views/MainViewProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+    vscode.window.showInformationMessage(l10n.t('hello'));
+    vscode.window.showInformationMessage(l10n.t('ciallo {0}', 'hiker'));
     const mainViewProvider = new MainViewProvider(context.extensionUri);
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider(
@@ -11,6 +13,26 @@ export function activate(context: vscode.ExtensionContext) {
             {webviewOptions: { retainContextWhenHidden: true }}
         ),
     );
+
+    const gotoSettings = vscode.commands.registerCommand('light-at.goto.settings', () => {
+        vscode.commands.executeCommand('workbench.action.openSettings', '@ext:himeditator.light-at');
+    });
+    context.subscriptions.push(gotoSettings);
+
+    const gotoConfig = vscode.commands.registerCommand('light-at.goto.config', () => {
+        vscode.window.showErrorMessage('Not implemented yet.');
+    });
+    context.subscriptions.push(gotoConfig);
+
+    const sessionsLoad = vscode.commands.registerCommand('light-at.load.sessions', () => {
+       vscode.window.showErrorMessage('Not implemented yet.');
+    });
+    context.subscriptions.push(sessionsLoad);
+
+    const chatNew = vscode.commands.registerCommand('light-at.chat.new', () => {
+        vscode.window.showErrorMessage('Not implemented yet.');
+    });
+    context.subscriptions.push(chatNew);
 }
 
 export function deactivate() {}
